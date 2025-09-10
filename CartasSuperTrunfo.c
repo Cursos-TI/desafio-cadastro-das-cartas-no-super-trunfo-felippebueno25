b51d8b8d65341672a531bf6d160d7f97da41fbda
Felippe Bueno <felippebueno25@gmail.com>
2025-09-10 18:39:30 +0000

    Adiciona cálculo de Super Poder e comparação de cartas no jogo Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -46,10 +46,15 @@ int main() {
     // Cálculo das densidades populacionais
     float densidadepop1 = (float) populacao1 / area1;
     float densidadepop2 = (float) populacao2 / area2;
+
     // Cálculo do PIB per capita
     // Fiquei confuso sobre como deveria entrar os dados do PIB para fazer o cálculo e tive de coverter pro número cheio pra realizar o cálculo do per capita
     float pibpercapita1 = (pib1 * 1000000000) / (float) populacao1;
     float pibpercapita2 = (pib2 * 1000000000) / (float) populacao2;
+
+    // Cálculo dos Super Poderes
+    float superPoder1 = (populacao1 + area1 + pib1 + pontosturi1 + pibpercapita1 + (1/densidadepop1));
+    float superPoder2 = (populacao2 + area2 + pib2 + pontosturi2 + pibpercapita2 + (1/densidadepop2));
 
     // Saída de Dados
     printf("\n\n>>> Carta 1:\n");
@@ -57,8 +62,17 @@ int main() {
            estado1, cod1, cidade1, populacao1, area1, pib1, pontosturi1, densidadepop1, pibpercapita1);
 
     printf("\n>>> Carta 2:\n");
-    printf("> Estado: %s\n> Código: %s\n> Nome da cidade: %s\n> População: %d habitantes\n> Área: %.2f Km²\n> PIB: %.2f bilhões de reais\n> Número de pontos turísticos: %d\n> Densidade Populacional %.2f hab/Km²\n> PIB per Capita: R$ %.2f\n",
-           estado2, cod2, cidade2, populacao2, area2, pib2, pontosturi2, densidadepop2, pibpercapita2);
+    printf("> Estado: %s\n> Código: %s\n> Nome da cidade: %s\n> População: %d habitantes\n> Área: %.2f Km²\n> PIB: %.2f bilhões de reais\n> Número de pontos turísticos: %d\n> Densidade Populacional %.2f hab/Km²\n> PIB per Capita: R$ %.2f\n", estado2, cod2, cidade2, populacao2, area2, pib2, pontosturi2, densidadepop2, pibpercapita2);
+
+    // Comparação de cartas
+    printf("\n\n[ Comparação de cartas ]\n");
+    printf("> População: Carta 1 venceu. %d\n", (populacao1 > populacao2));
+    printf("> Área: Carta 1 venceu. %d\n", (area1 > area2));
+    printf("> PIB: Carta 1 venceu. %d\n", (pib1 > pib2));
+    printf("> Pontos Turísticos: Carta 1 venceu. %d\n", (pontosturi1 > pontosturi2));
+    printf("> Densidade Populacional: Carta 1 venceu. %d\n", (densidadepop1 < densidadepop2));
+    printf("> PIB per capita: Carta 1 venceu. %d\n", (pibpercapita1 > pibpercapita2));
+    printf("> Super Poder: Carta 1 venceu. %d\n\n", (superPoder1 > superPoder2));
 
     return 0;
 }
